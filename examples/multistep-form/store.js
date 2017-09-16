a81221f077f8d4d8d38b4b0b9abec708df0b7e53
SigurdMW <sigurdmwahl@gmail.com>
2017-09-16 10:54:45 +0200

    updated example multistep form

--- examples/multistep-form/store.js
+++ examples/multistep-form/store.js
@@ -1,16 +1,15 @@
 function reducer(action, data, state){
   switch (action) {
-    case "SET_NAME":
-      return Object.assign({}, state, {name: data});
-    case "SET_EMAIL":
-      return Object.assign({}, state, {email: data});
-    case "SET_ADDRESS":
-      return Object.assign({}, state, {address: data});
-    case "SET_AGE":
-      return Object.assign({}, state, {age: data});
+    case "SET_STEP_1":
+      return Object.assign({}, state, {name: data.name, address: data.address});
+    case "SET_STEP_2":
+      return Object.assign({}, state, {email: data.email, age: data.age});
     case "COMPLETE_STEP":
-      const complete = state.completedSteps.concat(data);
-      return Object.assign({}, state, {completedSteps: complete});
+      if(!state.completedSteps.includes(data)){
+        const complete = state.completedSteps.concat(data);
+        return Object.assign({}, state, {completedSteps: complete});
+      }
+      return state;
     case "UNCOMPLETE_STEP":
       const uncomplete = state.completedSteps.filter(item => item !== data);
       return Object.assign({}, state, {completedSteps: uncomplete});
