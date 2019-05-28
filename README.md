89e59942086f91b7c8a401a0ba78a425cf1b11ab
Sigurd Wahl <sigurd.wahl@dnvgl.com>
2019-05-28 07:15:06 +0200

    core

--- README.md
+++ README.md
@@ -1,57 +1,29 @@
 # Sigdux
-A understandable and simple state management framework inspired by Flux and Redux. 
+https://codepen.io/mdd/pen/wGRqbw
 
-## Usage
-To set up your state container, you'll need a reducer and to create a new store and pass the reducer and default store values (optional).
+## Core
+Redux allow you to:
+* Commit changes to the store
+* Subscribe to changes in the store
 
-### Setup example
-```HTML
-<script src="sigdux.js"></script>
-<script>
-  function reducer(action, data, state){
-  	switch(action){
-  		case "SET_USERNAME":
-  			return Object.assign({}, state, {username: data});
-  		default:
-  			return state;
-  	}
-  }
+### Core - in depth
+Redux allow you to:
+* Commit changes to the store
+	* Action - intension to change the store. Dispatch = fire off an action
+	* Reducer - handles actions - what should happen when an action is dispatched?
+* Subscribe to changes in the store
 
-  const initialState = {
-  	user: null,
-  	count: 0
-  };
 
-  const store = new Sigdux(reducer, initialState);
-</script>
-```
+### Explained
+![Redux flow explained](https://cdn-images-1.medium.com/max/1200/0*cntBtPADjE2ykLSP.png)
 
-## Dispatch
 
+### What we'll build
+A small counter app, that has the actions INCREMENT and DECREMENT. The actions are sent to (dispatched) the reducer function, that knows what to do. What you return from the reducer is the new value of the entire state. We'll learn to hook it up to a button, and display the result in the DOM. 
+
+### Our store
 ```javascript
-store.dispatch("SET_USERNAME", "someusername");
-```
-
-## Get store
-You can get store at any point by running ´´´store.getState();´´´
-
-## Adding subscribers
-Subscribers are functions to run when the store gets updated. We have 2 types of subscribers; generic ones that run on all changes in state, and local ones that run when a specified part of the store gets updated.
-
-```javascript
-// Generic example
-store.addSubscriber(someFunction);
-
-// runs on all changes in store
-function someFunction(store){
-  console.log(store);
-}
-
-// Local example
-store.addSubscriber(someOtherFunction, ["username"]);
-
-// runs only when store.username gets changed
-function someOtherFunction(store){
-  console.log(store);
-}
+createStore() 	=> fn to hook up the reducers
+dispatch() 		=> fn to fire off actions
+subscribe() 	=> fn to subscribe to changes in the store
 ```
--- Sigdux.js
+++ Sigdux.js
@@ -1,23 +1,6 @@
-// https://github.com/reactjs/redux/blob/master/src/createStore.js
-
-// TODO: default values in reducers? right now they must be passed to the store on init
-// TODO: how to combine reducers?
-// TODO: subscribe to only a part of the state?
-// TODO: allow async reducers? with async/await or promises
-
-// Time travel poc
-
-// Consider deepEqual in state computation:
-// https://github.com/lodash/lodash/
-// https://github.com/substack/node-deep-equal
-
-// Optimize renderings:
-// 1. update state only when state changes.
-
-export class Sigdux {
+class Sigdux {
 	constructor(reducer, defaultStore) {
 		this.state = defaultStore || {};
-		this.previousState = {};
 		this.subscribers = [];
 		this.reducer = reducer;
 	}
@@ -28,36 +11,12 @@ export class Sigdux {
 
 	updateState(newState) {
 		const state = Object.assign({}, newState);
-
-		// fast and simple comparison - will not work with
-		// functions and DOM elements in obj
-		// Order of keys in obj matter
-		if (JSON.stringify(this.state) !== JSON.stringify(state)) {
-			this.previousState = this.state;
-			this.state = state;
-			this.updateSubscribers();
-		}
-	}
-
-	addReducer(action, data) {
-		if (typeof this.reducer === "function") {
-			this.updateState(this.reducer(action, data, this.state));
-		} else if (typeof this.reducer === "object" && this.reducer !== null) {
-			for (var i = 0; i < this.reducer.length; i++) {
-				if (typeof this.reducer[i] === "function") {
-					const reducer = this.reducer[i];
-					this.updateState(reducer(action, data, this.state));
-				} else {
-					throw new Error(typeof this.reducer[i] + " with index " + i + " in the array passed to the store setup is not a valid reducer. Reducer must be a function.");
-				}
-			}
-		} else {
-			throw new Error(typeof this.reducer + " passed to setup the store is not a valid reducer. Reducer must be either a function or an array of functions.");
-		}
+		this.state = state
+		this.updateSubscribers()
 	}
 
 	dispatch(action, data) {
-		this.addReducer(action, data);
+		this.updateState(this.reducer(action, data))
 	}
 
 	addSubscriber(func, subscriptions = []) {
@@ -73,19 +32,9 @@ export class Sigdux {
 
 	updateSubscribers() {
 		this.subscribers.map(subscriber => {
-			// update all generic subscriptions
-			// (subscribeers to entire store)
-			if (subscriber.subscriptions.length === 0) {
-				subscriber.func(this.state);
-			} else {
-				// update local subscriptions
-				// still passes entire state
-				subscriber.subscriptions.map(subscription => {
-					if (JSON.stringify(this.state[subscription]) !== JSON.stringify(this.previousState[subscription])) {
-						subscriber.func(this.state);
-					}
-				});
-			}
+			subscriber.func(this.state);
 		});
 	}
 }
+
+window.sigdux = Sigdux
